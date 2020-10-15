238db9189ed008f37e0897e0f385ef69d1f99213
Alumno <ulsa@D113.local>
2020-10-14 20:10:09 -0700

    alineaciones y constrains

---
+++ constrains/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  constrains
+//
+//  Created by Alumno on 10/14/20.
+//  Copyright © 2020 sebsstian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
